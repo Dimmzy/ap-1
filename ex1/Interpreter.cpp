a05c7ab45f4c227b4fbfb7fd99227463c8ba6787
dima <leetsushi@gmail.com>
2019-11-11 20:27:46 +0200

    Shunting Yard algorithm implementation

--- ex1/Interpreter.cpp
+++ ex1/Interpreter.cpp
@@ -1,5 +1,68 @@
 //
 // Created by dima on 11/11/2019.
 //
-
+#include <stack>
+#include <queue>
+#include <sstream>
 #include "Interpreter.h"
+
+Expression *Interpreter::interpret(string &str) {
+
+}
+
+void Interpreter::setVariables(string &vars) {
+
+}
+
+queue<string> Interpreter::ShuntingYard(string str) {
+  queue<string> postfix;
+  stack<string> s;
+  int i(0);
+  while (i < str.length()) {
+    string tempStr(1, str[i]);
+    if (isdigit(str[i])) {
+      string tempNum;
+      while ((isdigit(str[i]) || str[i] == '.') && i < str.length()) {
+        tempNum.push_back(str[i]);
+        i++;
+      }
+      postfix.push(tempNum);
+    } else if (isOperator(tempStr)) {
+      while (!s.empty() && precedence(s.top()) > precedence(tempStr)) {
+        postfix.push(s.top());
+        s.pop();
+      }
+      s.push(tempStr);
+      i++;
+    } else if (tempStr == "(") {
+      s.push(tempStr);
+      i++;
+    } else if (tempStr == ")") {
+      while (!s.empty() && s.top() != "(") {
+        postfix.push(s.top());
+        s.pop();
+      }
+      s.pop();
+      i++;
+    }
+  }
+  while (!s.empty()) {
+    postfix.push(s.top());
+    s.pop();
+  }
+  return postfix;
+}
+
+int Interpreter::precedence(const string &s) {
+  if (s == "*" || s == "/")
+    return 2;
+  if (s == "+" || s == "-")
+    return 1;
+  else
+    return 0;
+}
+
+bool Interpreter::isOperator(const string &s) {
+  return (s == "+" || s == "-" || s == "*" || s == "/");
+}
+
--- ex1/Interpreter.h
+++ ex1/Interpreter.h
@@ -6,15 +6,19 @@
 #define EX1__INTERPRETER_H_
 
 #include <string>
+#include <map>
 #include "Expression.h"
 class Interpreter {
  private:
-
+  static bool isOperator(const string& s);
+  static int precedence(const string& s);
+  map<string, int> varmap;
  public:
-  void setVariables(string vars);
-  Expression* interpret(string str);
+  void setVariables(string& vars);
+  Expression* interpret(string& str);
   Interpreter() = default;
   ~Interpreter();
+  static queue<string> ShuntingYard(string str);
 };
 
 #endif //EX1__INTERPRETER_H_
